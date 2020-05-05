b0d9b74dc301d7f7e93f0ecaf1d06bbfaf1795b0
alejanpa17 <alejandro170999@gmail.com>
2020-05-05 06:45:09 -0400

    Limpieza total

--- Practica 2/tut4/projectParams.py
+++
@@ -1,6 +0,0 @@
-# projectParams.py
-# ----------------
-
-STUDENT_CODE_DEFAULT = 'analysis.py,qlearningAgents.py,valueIterationAgents.py'
-PROJECT_TEST_CLASSES = 'reinforcementTestClasses.py'
-PROJECT_NAME = 'Project 3: Reinforcement learning'
